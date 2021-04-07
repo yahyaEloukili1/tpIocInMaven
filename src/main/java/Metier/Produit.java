9db42ddb89d3a19d20601267c6e579797334e7a8
yahya <yahya.ensas@hotmail.com>
2021-04-07 13:36:36 +0100

    unit tests tdd

---
+++ src/main/java/Metier/Produit.java
@@ -0,0 +1,10 @@
+package Metier;
+
+public class Produit {
+
+	public double produit(double a, double b) {
+		double c = a*b;
+		return c;
+	}
+
+}
---
+++ src/test/java/metier/TestProduit.java
@@ -0,0 +1,22 @@
+package metier;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Metier.Produit;
+
+public class TestProduit {
+
+	@Test
+	public void test() {
+		double a = 5;
+		double b = 8;
+		double expected = 40;
+		
+		Produit p = new Produit();
+		double res = p.produit(a,b);
+		assertEquals(expected, res,0.001);
+	}
+
+}
